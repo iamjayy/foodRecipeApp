30ad1b9aa0dd6cfe819735d2ef4337a3b4860b1b
jason <jason_doan94@yahoo.com>
2020-08-28 03:39:26 -0400

    navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App">recipe app</div>;
+  return (
+    <Router>
+      <main>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/recipes" exact component={Recipes} />
+          <Route path="/recipes/:id" component={SingleRecipe} />
+          <Route component={Default} />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "../images/logo.jpg";
+import { Link } from "react-router-dom";
+// import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="logo" style={{ height: "25%", width: "25%" }} />
+      </Link>
+      <div className="collapse navbar-collapse show ml-sm-5">
+        <ul className="navbar-nav">
+          <li className="navbar-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link className="nav-link" to="/recipes">
+              Recipes
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
